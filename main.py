b879bd0768fc67263d0ce6a97c9e72c466592cb5
Mohsin678 <mohsin79.h@gmail.com>
2025-07-18 11:01:15 +0530

    added various modules

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.data_ingestion import DataInestionPipeline
+from src.textSummarizer.pipeline.data_transformation import DataTransformationPipeline
+from src.textSummarizer.pipeline.model_trainer import ModelTrainerPipeline
+
 
 
 stage_name = "data ingestion stage"
@@ -7,6 +10,25 @@ try:
     logger.info(f"stage{stage_name} initiated")
     data_ingestion = DataInestionPipeline()
     data_ingestion.initiate_data_ingestion()
+    logger.info(f"stage{stage_name} completed")
+except Exception as e:
+    raise e
+
+
+stage_name = "data Transformation stage"
+try:
+    logger.info(f"stage{stage_name} initiated")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.initiate_data_transformation()
+    logger.info(f"stage{stage_name} completed")
+except Exception as e:
+    raise e
+
+stage_name = "Model Trainer stage"
+try:
+    logger.info(f"stage{stage_name} initiated")
+    Model_trainer = ModelTrainerPipeline()
+    Model_trainer.initiate_model_trainer()
     logger.info(f"stage{stage_name} completed")
 except Exception as e:
     raise e
--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -18,7 +18,7 @@ def read_yaml(path_to_yaml:Path)->ConfigBox:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
-    
+@ensure_annotations 
 def create_directories(path_to_directories:list, verbose=True):
     for path in path_to_directories:
         os.makedirs(path,exist_ok=True)
